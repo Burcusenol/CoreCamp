658a4dd9925caeddfe998875d9c256b8078d7b59
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-09-08 22:30:01 +0300

    Add project files.

---
+++ Entities/Concrete/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
+        public string AboutMapLocation { get; set; }
+        public bool AboutStatus { get; set; }
+
+    }
+}
